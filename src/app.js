3edfb1543032dec7095103523f14e8a97dccbe03
Abhishek Gupta <guptaabhishek60420@gmail.com>
2025-07-30 01:24:49 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 
 const app = express();
-// app.use("/",(req, res) => {
-//     res.send("Welcome to dashboard");
-// })
-
-app.use("/Hello",(req, res) => {
-    res.send("Hello world")
-})
-
-app.use("/abhi", (req,res) => {
-    res.send("Hello abhi");
+app.use("/user", (req, res, next) => {
+  console.log("user 1 is running");
+  // res.send("user 1 response");
+  next();
+}, 
+(req, res, next) => {
+  console.log("user 2 is running");
+  // res.send("user 2 response");
+  next();
+}, 
+(req, res, next) => {
+  console.log("user 3 is running");
+  // res.send("user 3 response");
+  next();
+}, 
+(req, res, next) => {
+  console.log("user 4 is running");
+  // res.send("user 4 response");
+  next();
+}, 
+(req, res, next) => {
+  console.log("user 5 is running");
+  // res.send("user 5 response");
+  // next();
 })
 
 app.listen(3000, () => {
